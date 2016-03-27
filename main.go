0a94302179b14f59cdad4f7a3582870a170ac0e3
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-03-27 12:55:31 +0800

    support android key and ios cert path flag.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 func main() {
 	version := flag.Bool("v", false, "gopush version")
 	confPath := flag.String("c", "", "yaml configuration file path for gopush")
+	certificateKeyPath := flag.String("i", "", "iOS certificate key file path for gopush")
+	apiKey := flag.String("k", "", "Android api key configuration for gopush")
 	port := flag.String("p", "", "port number for gopush")
 
 	flag.Parse()
@@ -37,6 +39,17 @@ func main() {
 	}
 
 	if gopush.PushConf.Ios.Enabled {
+
+		if *certificateKeyPath != "" {
+			gopush.PushConf.Ios.PemKeyPath = *certificateKeyPath
+		}
+
+		if gopush.PushConf.Ios.PemKeyPath == "" {
+			log.Println("iOS certificate path not define")
+
+			return
+		}
+
 		gopush.CertificatePemIos, err = certificate.FromPemFile(gopush.PushConf.Ios.PemKeyPath, "")
 
 		if err != nil {
@@ -49,6 +62,20 @@ func main() {
 			gopush.ApnsClient = apns.NewClient(gopush.CertificatePemIos).Production()
 		} else {
 			gopush.ApnsClient = apns.NewClient(gopush.CertificatePemIos).Development()
+		}
+	}
+
+	// check andorid api key exist
+	if gopush.PushConf.Android.Enabled {
+
+		if *apiKey != "" {
+			gopush.PushConf.Android.ApiKey = *apiKey
+		}
+
+		if gopush.PushConf.Android.ApiKey == "" {
+			log.Println("Android API Key not define")
+
+			return
 		}
 	}
 
