a41e63bcb77bf3b2cdd843c2eee30cc693c01fc6
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-04-01 15:45:24 +0800

    Add ios payload testing.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- gorush/notification.go
+++ gorush/notification.go
@@ -13,7 +13,21 @@ type ExtendJSON struct {
 	Value string `json:"val"`
 }
 
-type alert struct {
+const (
+	// PriorityLow will tell APNs to send the push message at a time that takes
+	// into account power considerations for the device. Notifications with this
+	// priority might be grouped and delivered in bursts. They are throttled, and
+	// in some cases are not delivered.
+	ApnsPriorityLow = 5
+
+	// PriorityHigh will tell APNs to send the push message immediately.
+	// Notifications with this priority must trigger an alert, sound, or badge on
+	// the target device. It is an error to use this priority for a push
+	// notification that contains only the content-available key.
+	ApnsPriorityHigh = 10
+)
+
+type Alert struct {
 	Action       string   `json:"action,omitempty"`
 	ActionLocKey string   `json:"action-loc-key,omitempty"`
 	Body         string   `json:"body,omitempty"`
@@ -48,12 +62,10 @@ type RequestPushNotification struct {
 	Topic    string       `json:"topic,omitempty"`
 	Badge    int          `json:"badge,omitempty"`
 	Sound    string       `json:"sound,omitempty"`
-	Expiry   int          `json:"expiry,omitempty"`
-	Retry    int          `json:"retry,omitempty"`
 	Category string       `json:"category,omitempty"`
 	URLArgs  []string     `json:"url-args,omitempty"`
 	Extend   []ExtendJSON `json:"extend,omitempty"`
-	Alert    alert        `json:"alert,omitempty"`
+	Alert    Alert        `json:"alert,omitempty"`
 
 	// meta
 	IDs []uint64 `json:"seq_id,omitempty"`
@@ -100,91 +112,102 @@ func pushNotification(notification RequestPushNotification) bool {
 	return success
 }
 
+func GetIOSNotification(req RequestPushNotification) *apns.Notification {
+	notification := &apns.Notification{}
+
+	if len(req.ApnsID) > 0 {
+			notification.ApnsID = req.ApnsID
+	}
+
+	if len(req.Topic) > 0 {
+		notification.Topic = req.Topic
+	}
+
+	if len(req.Priority) > 0 && req.Priority == "normal" {
+		notification.Priority = apns.PriorityLow
+	}
+
+	payload := payload.NewPayload().Alert(req.Message)
+
+	if req.Badge > 0 {
+		payload.Badge(req.Badge)
+	}
+
+	if len(req.Sound) > 0 {
+		payload.Sound(req.Sound)
+	}
+
+	if req.ContentAvailable {
+		payload.ContentAvailable()
+	}
+
+	if len(req.Extend) > 0 {
+		for _, extend := range req.Extend {
+			payload.Custom(extend.Key, extend.Value)
+		}
+	}
+
+	// Alert dictionary
+
+	if len(req.Alert.Title) > 0 {
+		payload.AlertTitle(req.Alert.Title)
+	}
+
+	if len(req.Alert.TitleLocKey) > 0 {
+		payload.AlertTitleLocKey(req.Alert.TitleLocKey)
+	}
+
+	// if len(req.Alert.LocArgs) > 0 {
+	// 	payload.AlertLocArgs(req.Alert.LocArgs)
+	// }
+
+	if len(req.Alert.TitleLocArgs) > 0 {
+		payload.AlertTitleLocArgs(req.Alert.TitleLocArgs)
+	}
+
+	if len(req.Alert.Body) > 0 {
+		payload.AlertBody(req.Alert.Body)
+	}
+
+	if len(req.Alert.LaunchImage) > 0 {
+		payload.AlertLaunchImage(req.Alert.LaunchImage)
+	}
+
+	if len(req.Alert.LocKey) > 0 {
+		payload.AlertLocKey(req.Alert.LocKey)
+	}
+
+	if len(req.Alert.Action) > 0 {
+		payload.AlertAction(req.Alert.Action)
+	}
+
+	if len(req.Alert.ActionLocKey) > 0 {
+		payload.AlertActionLocKey(req.Alert.ActionLocKey)
+	}
+
+	// General
+
+	if len(req.Category) > 0 {
+		payload.Category(req.Category)
+	}
+
+	if len(req.URLArgs) > 0 {
+		payload.URLArgs(req.URLArgs)
+	}
+
+	notification.Payload = payload
+
+	return notification
+}
+
 func pushNotificationIos(req RequestPushNotification) bool {
+
+	notification := GetIOSNotification(req)
 
 	// The Remote Notification Payload
 	// https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/TheNotificationPayload.html
 	for _, token := range req.Tokens {
-		notification := &apns.Notification{}
 		notification.DeviceToken = token
-
-		if len(req.ApnsID) > 0 {
-			notification.ApnsID = req.ApnsID
-		}
-
-		if len(req.Topic) > 0 {
-			notification.Topic = req.Topic
-		}
-
-		if len(req.Priority) > 0 && req.Priority == "normal" {
-			notification.Priority = apns.PriorityLow
-		}
-
-		payload := payload.NewPayload().Alert(req.Message)
-
-		if req.Badge > 0 {
-			payload.Badge(req.Badge)
-		}
-
-		if len(req.Sound) > 0 {
-			payload.Sound(req.Sound)
-		}
-
-		if req.ContentAvailable {
-			payload.ContentAvailable()
-		}
-
-		if len(req.Extend) > 0 {
-			for _, extend := range req.Extend {
-				payload.Custom(extend.Key, extend.Value)
-			}
-		}
-
-		// Alert dictionary
-
-		if len(req.Alert.Title) > 0 {
-			payload.AlertTitle(req.Alert.Title)
-		}
-
-		if len(req.Alert.TitleLocKey) > 0 {
-			payload.AlertTitleLocKey(req.Alert.TitleLocKey)
-		}
-
-		if len(req.Alert.LocArgs) > 0 {
-			payload.AlertTitleLocArgs(req.Alert.LocArgs)
-		}
-
-		if len(req.Alert.Body) > 0 {
-			payload.AlertBody(req.Alert.Body)
-		}
-
-		if len(req.Alert.LaunchImage) > 0 {
-			payload.AlertLaunchImage(req.Alert.LaunchImage)
-		}
-
-		if len(req.Alert.LocKey) > 0 {
-			payload.AlertLocKey(req.Alert.LocKey)
-		}
-
-		if len(req.Alert.Action) > 0 {
-			payload.AlertAction(req.Alert.Action)
-		}
-
-		if len(req.Alert.ActionLocKey) > 0 {
-			payload.AlertActionLocKey(req.Alert.ActionLocKey)
-		}
-
-		// General
-
-		if len(req.Category) > 0 {
-			payload.Category(req.Category)
-		}
-
-		if len(req.URLArgs) > 0 {
-			payload.URLArgs(req.URLArgs)
-		}
-
-		notification.Payload = payload
 
 		// send ios notification
 		res, err := ApnsClient.Push(notification)
