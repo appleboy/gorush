6994f7f3de6c94865babd687071312b5fea06507
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-04-01 10:35:04 +0800

    add api and push handler testing.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- gorush/server_test.go
+++ gorush/server_test.go
@@ -6,7 +6,10 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"testing"
+	"runtime"
 )
+
+var go_version = runtime.Version()
 
 func initTest() {
 	PushConf = BuildDefaultPushConf()
@@ -26,5 +29,37 @@ func TestRootHandler(t *testing.T) {
 
 			assert.Equal(t, "Welcome to notification server.", value)
 			assert.Equal(t, http.StatusOK, r.Code)
+		})
+}
+
+func TestAPIStatusHandler(t *testing.T) {
+	initTest()
+
+	r := gofight.New()
+
+	r.GET("/api/status").
+		Run(GetMainEngine(), func(r gofight.HttpResponse, rq gofight.HttpRequest) {
+			data := []byte(r.Body.String())
+
+			value, _ := jsonparser.GetString(data, "go_version")
+
+			assert.Equal(t, go_version, value)
+			assert.Equal(t, http.StatusOK, r.Code)
+		})
+}
+
+func TestPushHandler(t *testing.T) {
+	initTest()
+
+	r := gofight.New()
+
+	// missing some parameter.
+	r.POST("/api/push").
+		SetJSON(gofight.D{
+			"platform": 1,
+		}).
+		Run(GetMainEngine(), func(r gofight.HttpResponse, rq gofight.HttpRequest) {
+
+			assert.Equal(t, http.StatusBadRequest, r.Code)
 		})
 }
