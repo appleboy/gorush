4d46f7dbaba08fcb340e0f8b4fc0e1dd88d6b9a0
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-04-17 13:21:17 +0800

    [ci skip] update google doc.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- doc.go
+++ doc.go
@@ -4,17 +4,6 @@
 //
 //     https://github.com/appleboy/gorush
 //
-// Support Google Cloud Message using go-gcm library for Android.
-// Support HTTP/2 Apple Push Notification Service using apns2 library.
-// Support YAML configuration.
-// Support command line to send single Android or iOS notification.
-// Support Web API to send push notification.
-// Support zero downtime restarts for go servers using endless.
-// Support HTTP/2 or HTTP/1.1 protocol.
-// Support notification queue and multiple workers.
-// Support /api/stat/app show notification success and failure counts.
-// Support /api/config show your yml config.
-//
 // The pre-compiled binaries can be downloaded from release page.
 //
 // Send Android notification
@@ -36,13 +25,6 @@
 // Get go status of api server using httpie tool:
 //
 //   $ http -v --verify=no --json GET https://localhost:8088/api/stat/go
-//
-// Gorush support the following API.
-//
-// GET /api/stat/go Golang cpu, memory, gc, etc information. Thanks for golang-stats-api-handler.
-// GET /api/stat/app show notification success and failure counts.
-// GET /api/config show server yml config file.
-// POST /api/push push ios and android notifications.
 //
 // Simple send iOS notification example, the platform value is 1:
 //
