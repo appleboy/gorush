12505bd17d579e3d0b78a454ab956dd77e8c68a5
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-05-20 16:35:48 +0800

    replace go path with global variable.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- docker/testing.sh
+++ docker/testing.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-cd /go/src/github.com/appleboy/gorush
+cd ${GOPATH}/src/github.com/appleboy/gorush
 sed -i"" -e "s/localhost/redis/g" config/config.yml
 sed -i"" -e "s/localhost/redis/g" gorush/status_test.go
 sed -i"" -e "s/localhost/redis/g" storage/redis/redis_test.go
