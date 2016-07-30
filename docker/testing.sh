5b67226b51588128877492e95ae8fca812c28bf5
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-07-30 23:02:43 +0800

    remove project path.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- docker/testing.sh
+++ docker/testing.sh
@@ -3,10 +3,11 @@
 
 set -e
 
-cd ${GOPATH}/src/github.com/appleboy/gorush
 sed -i"" -e "s/localhost/redis/g" config/config.yml
 sed -i"" -e "s/localhost/redis/g" config/config.go
 sed -i"" -e "s/localhost/redis/g" config/config_test.go
 sed -i"" -e "s/localhost/redis/g" gorush/status_test.go
 sed -i"" -e "s/localhost/redis/g" storage/redis/redis_test.go
+
+echo "install package and testing code coverage."
 make install && coverage all
