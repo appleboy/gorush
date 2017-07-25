abe4d8470137f75bfb298eaabb81e75d6844db97
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-07-25 15:36:26 +0800

    fix testing
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- gorush/notification_fcm_test.go
+++ gorush/notification_fcm_test.go
@@ -1,6 +1,7 @@
 package gorush
 
 import (
+	"os"
 	"testing"
 
 	"github.com/appleboy/gorush/config"
@@ -15,4 +16,97 @@ func TestMissingKeyForInitFCMClient(t *testing.T) {
 	assert.Nil(t, client)
 	assert.Error(t, err)
 	assert.Equal(t, "Missing Android API Key", err.Error())
+}
+
+func TestPushToAndroidWrongAPIKey(t *testing.T) {
+	PushConf = config.BuildDefaultPushConf()
+
+	PushConf.Android.Enabled = true
+	PushConf.Android.APIKey = os.Getenv("ANDROID_API_KEY") + "a"
+
+	req := PushNotification{
+		Tokens:   []string{"aaaaaa", "bbbbb"},
+		Platform: PlatFormAndroid,
+		Message:  "Welcome",
+	}
+
+	// FCM server send message error: 401 error: 401 Unauthorized
+	err := PushToAndroid(req)
+	assert.False(t, err)
+}
+
+func TestPushToAndroidWrongToken(t *testing.T) {
+	PushConf = config.BuildDefaultPushConf()
+
+	PushConf.Android.Enabled = true
+	PushConf.Android.APIKey = os.Getenv("ANDROID_API_KEY")
+
+	req := PushNotification{
+		Tokens:   []string{"aaaaaa", "bbbbb"},
+		Platform: PlatFormAndroid,
+		Message:  "Welcome",
+	}
+
+	// FCM server send message error: 401 error: 401 Unauthorized
+	err := PushToAndroid(req)
+	assert.False(t, err)
+}
+
+func TestPushToAndroidRightTokenForJSONLog(t *testing.T) {
+	PushConf = config.BuildDefaultPushConf()
+
+	PushConf.Android.Enabled = true
+	PushConf.Android.APIKey = os.Getenv("ANDROID_API_KEY")
+	// log for json
+	PushConf.Log.Format = "json"
+
+	androidToken := os.Getenv("ANDROID_TEST_TOKEN")
+
+	req := PushNotification{
+		Tokens:   []string{androidToken},
+		Platform: PlatFormAndroid,
+		Message:  "Welcome",
+	}
+
+	isError := PushToAndroid(req)
+	assert.False(t, isError)
+}
+
+func TestPushToAndroidRightTokenForStringLog(t *testing.T) {
+	PushConf = config.BuildDefaultPushConf()
+
+	PushConf.Android.Enabled = true
+	PushConf.Android.APIKey = os.Getenv("ANDROID_API_KEY")
+
+	androidToken := os.Getenv("ANDROID_TEST_TOKEN")
+
+	req := PushNotification{
+		Tokens:   []string{androidToken},
+		Platform: PlatFormAndroid,
+		Message:  "Welcome",
+	}
+
+	isError := PushToAndroid(req)
+	assert.False(t, isError)
+}
+
+func TestOverwriteAndroidAPIKey(t *testing.T) {
+	PushConf = config.BuildDefaultPushConf()
+
+	PushConf.Android.Enabled = true
+	PushConf.Android.APIKey = os.Getenv("ANDROID_API_KEY")
+
+	androidToken := os.Getenv("ANDROID_TEST_TOKEN")
+
+	req := PushNotification{
+		Tokens:   []string{androidToken, "bbbbb"},
+		Platform: PlatFormAndroid,
+		Message:  "Welcome",
+		// overwrite android api key
+		APIKey: "1234",
+	}
+
+	// FCM server error: 401 error: 401 Unauthorized
+	err := PushToAndroid(req)
+	assert.False(t, err)
 }
