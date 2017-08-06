2bcc17de517c2956a65a389d8d1e31564269d718
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-08-06 09:53:38 +0800

    check response header. (#266)
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- gorush/server_test.go
+++ gorush/server_test.go
@@ -101,6 +101,7 @@ func TestRootHandler(t *testing.T) {
 
 			assert.Equal(t, "Welcome to notification server.", value)
 			assert.Equal(t, http.StatusOK, r.Code)
+			assert.Equal(t, "application/json; charset=utf-8", r.HeaderMap.Get("Content-Type"))
 		})
 }
 
@@ -160,6 +161,7 @@ func TestMissingNotificationsParameter(t *testing.T) {
 		Run(routerEngine(), func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
 
 			assert.Equal(t, http.StatusBadRequest, r.Code)
+			assert.Equal(t, "application/json; charset=utf-8", r.HeaderMap.Get("Content-Type"))
 		})
 }
 
