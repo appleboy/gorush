74082ff8fa65e2a87a2b85f718a60289c966b345
jrxjsj <juan.sanchez@jaraxa.com>
2017-11-21 17:02:57 +0100

    Added test draft for webpush

---
+++ gorush/notification_web_test.go
@@ -0,0 +1,52 @@
+package gorush
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/appleboy/gorush/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	PushConf, _ = config.LoadConf("")
+	if err := InitLog(); err != nil {
+		log.Fatal(err)
+	}
+
+	InitWorkers(PushConf.Core.WorkerNum, PushConf.Core.QueueNum)
+
+	if err := InitAppStatus(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestMissingWebAPIKey(t *testing.T) {
+	PushConf, _ = config.LoadConf("")
+
+	PushConf.Web.Enabled = true
+	PushConf.Web.APIKey = ""
+
+	err := CheckPushConf()
+
+	assert.Error(t, err)
+	assert.Equal(t, "Missing GCM API Key for Chrome", err.Error())
+}
+
+func TestPushToWebWrongSubscription(t *testing.T) {
+	PushConf, _ = config.LoadConf("")
+
+	PushConf.Web.Enabled = true
+	PushConf.Web.APIKey = os.Getenv("ANDROID_API_KEY")
+
+	req := PushNotification{
+		Subscription: [...]Subscription{{"aaaaaa","bbbbb","ccccc"}},
+		Platform:     PlatformWeb,
+		Message:      "Welcome",
+	}
+
+	// Web Success count: 0, Failure count: 1
+	isError := PushToWeb(req)
+	assert.True(t, isError)
+}
