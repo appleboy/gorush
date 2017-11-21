69e56e0890f72a25bda7cc1709827a19e3dbdf51
jrxjsj <juan.sanchez@jaraxa.com>
2017-11-21 17:11:36 +0100

    Fixed test draft for webpush

--- gorush/notification_web_test.go
+++ gorush/notification_web_test.go
@@ -41,7 +41,7 @@ func TestPushToWebWrongSubscription(t *testing.T) {
 	PushConf.Web.APIKey = os.Getenv("ANDROID_API_KEY")
 
 	req := PushNotification{
-		Subscriptions: [...]Subscription{{"aaaaaa", "bbbbb", "ccccc"}},
+		Subscriptions: [1]Subscription{{"aaaaaa", "bbbbbb", "cccccc"}},
 		Platform:      PlatformWeb,
 		Message:       "Welcome",
 	}
