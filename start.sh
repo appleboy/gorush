01ce909ad5a45fa2b2728f04220832a9b5377e29
Greg Slomin <gslomin@eagleeyenetworks.com>
2018-05-22 13:40:59 -0500

    Typo in startup script

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ echo $EEN_IOS_PROD_CERT | base64 -d > ios_prod.p12
 
 if [ -z $IOS_PRODUCTION_MODE ];
 then
- 	./bin/gorush -c config/config.yml -K $EEN_ANDROID_API_KEY --production
+ 	./bin/gorush -c config/config.yml -k $EEN_ANDROID_API_KEY --production
 else
 	./bin/gorush -c config/config.yml -k $EEN_ANDROID_API_KEY
 fi
