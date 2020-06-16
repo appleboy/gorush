bc10d4fd04b0a2b7b94e10ff694f10e14a77c285
Emmet McPoland <emcpoland01@qub.ac.uk>
2020-06-16 13:32:39 +0100

    Removed debugging

--- gorush/notification_apns.go
+++ gorush/notification_apns.go
@@ -5,7 +5,6 @@ import (
 	"crypto/tls"
 	"encoding/base64"
 	"errors"
-	"fmt"
 	"net"
 	"net/http"
 	"path/filepath"
@@ -392,11 +391,7 @@ Retry:
 			deviceNotification.DeviceToken = token
 
 			// send ios notification
-			url := fmt.Sprintf("before .../3/device/%v", deviceNotification.DeviceToken)
-			fmt.Println(url)
 			res, err := client.Push(&deviceNotification)
-			url = fmt.Sprintf("after .../3/device/%v", deviceNotification.DeviceToken)
-			fmt.Println(url)
 
 			if err != nil || res.StatusCode != 200 {
 				if err == nil {
