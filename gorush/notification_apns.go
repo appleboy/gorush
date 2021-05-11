36a91f1597a979fa3cd391be62ed17801796b5fc
Jason Murray <jason@chaosaffe.io>
2021-05-11 10:11:27 -0700

    fix linting issues

--- gorush/notification_apns.go
+++ gorush/notification_apns.go
@@ -161,9 +161,10 @@ func newApnsClient(certificate tls.Certificate) (*apns2.Client, error) {
 
 	if h2Transport, err := http2.ConfigureTransports(transport); err != nil {
 		return nil, err
-	} else {
-		configureHTTP2ConnHealthCheck(h2Transport)
 	}
+
+	configureHTTP2ConnHealthCheck(h2Transport)
+
 	client.HTTPClient.Transport = transport
 
 	return client, nil
@@ -190,9 +191,9 @@ func newApnsTokenClient(token *token.Token) (*apns2.Client, error) {
 
 	if h2Transport, err := http2.ConfigureTransports(transport); err != nil {
 		return nil, err
-	} else {
-		configureHTTP2ConnHealthCheck(h2Transport)
 	}
+
+	configureHTTP2ConnHealthCheck(h2Transport)
 
 	client.HTTPClient.Transport = transport
 
