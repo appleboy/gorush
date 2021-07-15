4be992acee45ff713664302fe9a638fdb0e7c1e7
Bo-Yi Wu <appleboy.tw@gmail.com>
2021-07-16 00:36:20 +0800

    update
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- queue/queue.go
+++ queue/queue.go
@@ -4,6 +4,7 @@ import (
 	"runtime"
 
 	"github.com/appleboy/gorush/config"
+	"github.com/appleboy/gorush/logx"
 	"github.com/appleboy/gorush/queue/simple"
 )
 
@@ -79,9 +80,9 @@ func (q *Queue) startWorker() {
 	for i := 0; i < q.workerCount; i++ {
 		go func(num int) {
 			q.routineGroup.Run(func() {
-				// logx.LogAccess.Info("started the worker num ", num)
+				logx.LogAccess.Info("started the worker num ", num)
 				q.worker.Run(q.quit)
-				// logx.LogAccess.Info("closed the worker num ", num)
+				logx.LogAccess.Info("closed the worker num ", num)
 			})
 		}(i)
 	}
