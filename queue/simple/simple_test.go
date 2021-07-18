fa1f89da918a86bd88d60040c33cac88173dbd5d
Bo-Yi Wu <appleboy.tw@gmail.com>
2021-07-18 12:47:00 +0800

    refactor(queue): update run func in simple channel

--- queue/simple/simple_test.go
+++ queue/simple/simple_test.go
@@ -1,13 +1,38 @@
 package simple
 
 import (
+	"log"
 	"runtime"
 	"testing"
+	"time"
 
 	"github.com/appleboy/gorush/gorush"
+	"github.com/appleboy/gorush/logx"
+	"github.com/appleboy/gorush/queue"
 
 	"github.com/stretchr/testify/assert"
 )
+
+type mockMessage struct {
+	msg string
+}
+
+func (m mockMessage) Bytes() []byte {
+	return []byte(m.msg)
+}
+
+func TestMain(m *testing.M) {
+	if err := logx.InitLog(
+		"debug",
+		"stdout",
+		"debug",
+		"stdout",
+	); err != nil {
+		log.Fatalf("Can't load log module, error: %v", err)
+	}
+
+	m.Run()
+}
 
 func TestQueueUsage(t *testing.T) {
 	w := NewWorker()
@@ -32,4 +57,28 @@ func TestMaxCapacity(t *testing.T) {
 
 	err := w.Queue(&gorush.PushNotification{})
 	assert.Equal(t, errMaxCapacity, err)
+}
+
+func TestCustomFuncAndWait(t *testing.T) {
+	m := mockMessage{
+		msg: "foo",
+	}
+	w := NewWorker(
+		WithRunFunc(func(msg queue.QueuedMessage) error {
+			logx.LogAccess.Infof("get message: %s", msg.Bytes())
+			time.Sleep(500 * time.Millisecond)
+			return nil
+		}),
+	)
+	q := queue.NewQueue(w, 2)
+	q.Start()
+	time.Sleep(100 * time.Millisecond)
+	q.Queue(m)
+	q.Queue(m)
+	q.Queue(m)
+	q.Queue(m)
+	time.Sleep(600 * time.Millisecond)
+	q.Shutdown()
+	q.Wait()
+	// you will see the execute time > 1000ms
 }
